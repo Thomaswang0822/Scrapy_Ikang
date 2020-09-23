2c79bc8d943d2279268cb3f549f8d1dd7db3efc3
HaoxuanWang_OldB <56232866+Thomaswang0822@users.noreply.github.com>
2020-09-23 16:46:23 +0800

    Update ikangSpider.py

--- ikangSpider.py
+++ ikangSpider.py
@@ -1,15 +1,9 @@
 import scrapy
-from scrapy.selector import Selector
-from scrapy.loader import ItemLoader
 from ikang.items import IkangItem
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import os
 import time
 from selenium.webdriver.common.action_chains import ActionChains
-import requests
-
-basedir = os.path.dirname(os.path.realpath('__file__'))
 
 
 class IkangspiderSpider(scrapy.Spider):
@@ -18,10 +12,20 @@ class IkangspiderSpider(scrapy.Spider):
     start_urls = ['http://mall.ikang.com/searchList?product_first_type_id=232']
     handle_httpstatus_list = [301]
 
+    ''' Wrap up a function
+    Workflow:
+    click the proudct element (good) in our loop
+    swtich to the new tab
+    find all the elements by xpath
+    extract all the texts and concatenate them into a string
+    close the tab
+    switch back to the primary tab 
+    return the string
+    '''
     def getDetail(self, driver, good):
         ActionChains(driver).move_to_element(good).click().perform()
         driver.switch_to.window(driver.window_handles[-1])
-        time.sleep(1)  #
+        time.sleep(1)  # pause for driver to load the page
         exam_list = driver.find_elements_by_xpath('//table/tbody/tr/td/tr/td[1]')
         exam_str = ''
         for exam in exam_list:
@@ -39,18 +43,18 @@ class IkangspiderSpider(scrapy.Spider):
 
         # initialize driver
         driver = webdriver.Chrome(chrome_options=chrome_options)
-        driver.implicitly_wait(1)
+        driver.implicitly_wait(1) # pause to cheat anti-scrawl mech
         url = 'http://mall.ikang.com/searchList?product_first_type_id=232'
         driver.get(url)
         driver.maximize_window()    # to see all elements
 
         js = "var q=document.documentElement.scrollTop=800"
-        # 此处要用sleep， implicitly wait不管用
+        # use sleep()， implicitly wait doesn't work
         time.sleep(1) #
         driver.execute_script(js)
         time.sleep(1) #
 
-        # 初始页面：第一页
+        # handle first page
         goods_list = driver.find_elements_by_xpath('//div[@class="goods-card-right"]')
         for i in range( len(goods_list) ):
             ikang_item = IkangItem()
@@ -58,13 +62,12 @@ class IkangspiderSpider(scrapy.Spider):
             ikang_item['product'] = good.find_element_by_xpath('./p').text
             ikang_item['price'] = good.find_element_by_xpath('./div[1]/div[1]').text
             ikang_item['sales'] = good.find_element_by_xpath('./div[1]/div[2]').text
-            ###
+            ### use the helper function
             ikang_item['exams'] = self.getDetail(driver=driver, good=good)
-            ###
             yield ikang_item
 
 
-        # 点击5次button，重复操作，得到2-6页的信息
+        # looping, click button 5 times, get data in page 2-6
         for i in range(5):
             driver.implicitly_wait(1)
             driver.find_element_by_xpath('//button[@class="btn-next"]').click()
@@ -77,6 +80,7 @@ class IkangspiderSpider(scrapy.Spider):
                 ikang_item['product'] = good.find_element_by_xpath('./p').text
                 ikang_item['price'] = good.find_element_by_xpath('./div[1]/div[1]').text
                 ikang_item['sales'] = good.find_element_by_xpath('./div[1]/div[2]').text
+                ### use the helper function
                 ikang_item['exams'] = self.getDetail(driver=driver, good=good)
                 yield ikang_item
 
